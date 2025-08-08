eeb6e4cf04f23d08944589a21b5af8250d7cbf94
Muhammad Usman <usman853136@gmail.com>
2025-08-08 17:01:42 +0500

    feat: add request validation middleware using Zod for schema validation

---
+++ 1/src/helpers/validators.ts
@@ -0,0 +1,29 @@
+import type { NextFunction, Request, Response } from "express";
+import { ZodError, type ZodType } from "zod";
+import { BadRequestError } from "../core/CustomError.js";
+
+export enum ValidationSource {
+    BODY = "body",
+    QUERY = "query",
+    HEADER = "header",
+    PARAMS = "params"
+}
+
+const validateRequest = (schema: ZodType, source: ValidationSource) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const data = schema.parse(req[source])
+            Object.assign(req[source], data)
+            next()
+        } catch (error) {
+
+            if (error instanceof ZodError) {
+                const message = (error as ZodError).issues.map(e => e.message).join(", ");
+                return next(new BadRequestError(message))
+            }
+            next(error)
+        }
+    }
+}
+
+export default validateRequest
